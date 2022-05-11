cf9721ff831e6e653105c03b8e0a1384cb93ef62
Fábio de Paula Guedes Florentino <fabio7guedes@gmail.com>
2022-05-11 00:21:00 -0300

    Seção 2: DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+                new Evento() {
+                    EventoId = 1,
+                    Tema = "Angular 11 e .Net 5",
+                    local = "Belo Horizonte",
+                    Lote = "1º Lote",
+                    QtdePessoas = 250,
+                    DataEvento = DateTime.Now.Date.ToString("dd/MM/yyyy")
+            },
+                new Evento() {
+                    EventoId = 2,
+                    Tema = "Angular 11 e suas novidades",
+                    local = "São Paulo",
+                    Lote = "2º Lote",
+                    QtdePessoas = 250,
+                    DataEvento = DateTime.Now.Date.AddDays(3).ToString("dd/MM/yyyy")
+                }
+            };
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(w => w.EventoId.Equals(id));
+        }
+    }
+}
---
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -0,0 +1,14 @@
+namespace ProEventos.API.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdePessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemURL { get; set; }
+
+    }
+}
